73cbf514bc2547668436c8ffb08f4b40e4ed2960
ivan-alfer <ivan.alfer0206@gmail.com>
2019-05-30 17:47:34 +0300

    create a sub folder of this project

--- src/main/java/com/apartmentseller/apartmentseller/repository/AnnouncementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apartmentseller.apartmentseller.repository;
-
-import com.apartmentseller.apartmentseller.domain.Announcement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-}
--- src/main/java/com/apartmentseller/apartmentseller/services/exceptions/AnnouncementNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.apartmentseller.apartmentseller.services.exceptions;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
-public class AnnouncementNotFoundException extends RuntimeException {
-}
